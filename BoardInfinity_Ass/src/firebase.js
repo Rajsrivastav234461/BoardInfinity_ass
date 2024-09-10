874a529aac120f5c5b5de59288a8c6bca10455ae
Nitesh Kumar Srivastava <srivastavnitesh513@gmail.com>
2024-09-10 11:47:47 +0530

    Update firebase.js

--- BoardInfinity_Ass/src/firebase.js
+++ BoardInfinity_Ass/src/firebase.js
@@ -1,18 +1,20 @@
- // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
- 
-  const firebaseConfig = {
-  apiKey: "AIzaSyAq_c91IiYanTuwiP7ZPDCfA5L8XyQfaH0",
-  authDomain: "biardinfinity.firebaseapp.com",
-  projectId: "biardinfinity",
-  storageBucket: "biardinfinity.appspot.com",
-  messagingSenderId: "589561284696",
-  appId: "1:589561284696:web:bda9017972d0f100e9622d",
-  measurementId: "G-FNWQWV46YQ"
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Firestore
+export const db = getFirestore(app);
